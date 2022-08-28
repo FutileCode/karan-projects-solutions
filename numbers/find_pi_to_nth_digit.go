05f3b1ab362b00ed4df34f5df282e61c35030c34
FutileCode <99463618+FutileCode@users.noreply.github.com>
2022-08-28 01:39:07 +0100

    Add files via upload

--- numbers/find_pi_to_nth_digit.go
+++ numbers/find_pi_to_nth_digit.go
@@ -1,3 +1,9 @@
+/*
+Find PI to the Nth Digit - Enter a number and have the
+program generate PI up to that many decimal places.
+Keep a limit to how far the program will go.
+*/
+
 package main
 
 import (
